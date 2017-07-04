866d71fedb93b9b76b8b102f36c0198e16c3ebb9
neotec <neotec@gmail.com>
2017-07-04 16:09:43 +0800

    added contract folder and config file

---
+++ config-example.js
@@ -0,0 +1,4 @@
+// This is the configuration file to store information such as public key and private key etc.
+
+var ethAddress = '[Your Ethereum Address]';
+var ethKey = '[Your Ethereum Private Key]';
---
+++ contract/deployContract.js
@@ -0,0 +1,5 @@
+var Web3 = require('web3');
+var util = require('ethereumjs-util');
+var tx = require('ethereumjs-tx');
+var lightwallet = require('eth-lightwallet');
+var txutils = lightwallet.txutils;
